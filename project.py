c7d6b9013c961b902bc2c7d712cd38c6870a8572
AndrewManko <a.manko@g.nsu.ru>
2018-11-25 00:43:25 +0700

    finish

--- project.py
+++ project.py
@@ -1,4 +1,13 @@
+def simple(x):
+    for i in range(2,x):
+        b = True
+        for j in range(2,i):
+            if i % j == 0:
+                b = False
+                break
+        if b:
+            print(i)
+
 with open('input.txt') as f_in:
-    text = f_in.read()
-print(text)
-print()
+    text = int(f_in.read())
+simple(text)
